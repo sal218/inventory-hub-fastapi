67c7a5a9877b67e00a87635eb15fb93fde36b3b4
sal218 <salman.mourad@student.ufv.ca>
2025-03-20 04:36:01 +0000

    1. Updated main.py to account for ui.py route. 2. fixed typos in index.html, 3. added favicon.ico href to base.html

--- app/main.py
+++ app/main.py
@@ -1,13 +1,17 @@
 from fastapi import FastAPI
 from app.database import engine, Base
-from app.routes import items, categories, suppliers, auth
+from fastapi.staticfiles import StaticFiles
+from app.routes import items, categories, suppliers, auth, ui
 
 
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Inventory Management System API")
 
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
 app.include_router(items.router, prefix="/items", tags=["Items"])
 app.include_router(categories.router, prefix="/categories", tags=["Categories"])
 app.include_router(suppliers.router, prefix="/suppliers", tags=["Suppliers"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
+app.include_router(ui.router, prefix="", tags=["UI"])
