5d1ccee450828faab3e1e6bbaf26d2bb04e09343
sal218 <salman.mourad@student.ufv.ca>
2025-04-08 02:21:38 +0000

    updated dashboard button look to match other buttons in nav bar

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from app.database import engine, Base
 from fastapi.staticfiles import StaticFiles
-from app.routes import items, categories, suppliers, auth, ui, oauth
+from app.routes import items, categories, suppliers, auth, ui, oauth, api_dashboard
 
 
 Base.metadata.create_all(bind=engine)
@@ -16,3 +16,4 @@ app.include_router(suppliers.router, prefix="/suppliers", tags=["Suppliers"])
 app.include_router(auth.router, prefix="/auth", tags=["Authentication"])
 app.include_router(ui.router, prefix="", tags=["UI"])
 app.include_router(oauth.router, tags=["OAuth"])
+app.include_router(api_dashboard.router, tags=["API"])
