89859c85f43b3aed2988f74b666e7b58fc8cb1f4
sal218 <salman.mourad@student.ufv.ca>
2025-04-11 05:38:51 +0000

    Added global exception handler that checks for 401 error code (when token expires) and reroutes to login page instead of just displaying default JSON error message

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request, HTTPException, status
+from fastapi.responses import RedirectResponse, JSONResponse
 from app.database import engine, Base
 from fastapi.staticfiles import StaticFiles
 from app.routes import items, categories, suppliers, auth, ui, oauth, api_dashboard
@@ -7,6 +8,14 @@ from app.routes import items, categories, suppliers, auth, ui, oauth, api_dashbo
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI(title="Inventory Management System API")
+
+
+# global exception handler to redirect 401 errors back to the login page
+@app.exception_handler(HTTPException)
+async def custom_http_exception_hanlder(request: Request, exc: HTTPException):
+    if exc.status_code == status.HTTP_401_UNAUTHORIZED:
+        return RedirectResponse(url="/login")   
+    return JSONResponse(status_code=exc.status_code, content={"detail" : exc.detail})
 
 app.mount("/static", StaticFiles(directory="app/static"), name="static")
 
